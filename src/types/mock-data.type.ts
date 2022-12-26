40c2ab7419e9d683be55379ac91a6cf2e7afc7c8
hinto <hinto@rambler.ru>
2022-12-26 18:00:46 +0700

    Добовляет генератор моковых данных

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,11 @@
+export type MockData = {
+  names: string[];
+  descriptions: string[];
+  genres: string[];
+  previewVideoLinks: string[];
+  videoLinks: string[];
+  actors: string[];
+  producers: string[];
+  posterImages: string[];
+  backgroundImages: string[];
+};
