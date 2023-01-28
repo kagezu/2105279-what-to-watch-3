d3f5042e9fafeb4398cb6062927a3ea11d59494f
hinto <hinto@rambler.ru>
2023-01-28 15:48:52 +0700

    Добавляет dto для обновления фильма

---
+++ src/modules/film/dto/update-film.dto.ts
@@ -0,0 +1,17 @@
+import { Genre } from '../../../types/genre.type.js';
+
+export default class UpdateFilmDto {
+  name?: string;
+  description?: string;
+  publicationDate?: string;
+  genre?: Genre;
+  released?: string;
+  previewVideoLink?: string;
+  videoLink?: string;
+  actors?: string[];
+  producer?: string;
+  runTime?: number;
+  posterImage?: string;
+  backgroundImage?: string;
+  color?: string;
+}
