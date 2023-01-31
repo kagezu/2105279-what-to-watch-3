d2f8e07ec9d4579774d058053192cdd4fdac4879
hinto <hinto@rambler.ru>
2023-01-31 19:23:59 +0700

    Добавляет обработку ошибок

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,11 +5,19 @@ import { LoggerInterface } from '../../common/logger/logger.interface.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
 import { Request, Response } from 'express';
 import CreateUserDto from './dto/create-user.dto.js';
+import { UserServiceInterface } from './user-service.interface.js';
+import HttpError from '../../common/errors/http-error.js';
+import { StatusCodes } from 'http-status-codes';
+import { fillDTO } from '../../utils/common.js';
+import UserResponse from './response/user.response.js';
+import { ConfigInterface } from '../../common/config/config.interface.js';
 
 @injectable()
 export default class UserController extends Controller {
   constructor(
     @inject(Component.LoggerInterface) logger: LoggerInterface,
+    @inject(Component.UserServiceInterface) private readonly userService: UserServiceInterface,
+    @inject(Component.ConfigInterface) private readonly configService: ConfigInterface,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -17,10 +25,25 @@ export default class UserController extends Controller {
     this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
   }
 
-  public create(
-    _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    _res: Response
-  ): void {
-    throw new Error('[UserController] Oops');
+  public async create(
+    { body }: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
+    res: Response,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.send(
+      res,
+      StatusCodes.CREATED,
+      fillDTO(UserResponse, result)
+    );
   }
 }
