d98496ab6def6f25b64c37fe5f0fbc25dbd1a748
hinto <hinto@rambler.ru>
2023-02-10 19:58:38 +0700

    добавляет методы для проверки пароля и токенов

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,6 +5,7 @@ import CreateUserDto from './dto/create-user.dto.js';
 import { UserServiceInterface } from './user-service.interface.js';
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
 import { Component } from '../../types/component.types.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -42,5 +43,19 @@ export default class UserService implements UserServiceInterface {
   public async exists(documentId: string): Promise<boolean> {
     return (await this.userModel
       .exists({ _id: documentId })) !== null;
+  }
+
+  public async verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null> {
+    const user = await this.findByEmail(dto.email);
+
+    if (!user) {
+      return null;
+    }
+
+    if (user.verifyPassword(dto.password, salt)) {
+      return user;
+    }
+
+    return null;
   }
 }
